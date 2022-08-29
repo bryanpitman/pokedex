35d00aabba98c24ecf1a74afc6ca13cd5c4661c8
Bryan Pitman <pitman.bryan@gmail.com>
2022-08-29 15:42:19 -0700

    pokedex complete

---
+++ src/Pokecard.js
@@ -0,0 +1,11 @@
+import {pokemons, Pokedex } from "./Pokedex"
+
+/** return a single card  */
+function Pokecard(pokemons){
+  
+
+  https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png.
+
+}
+
+export default Pokecard
